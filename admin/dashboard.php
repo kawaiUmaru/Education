36aa463c8e0876901556e8b61e02daf633a1ef14
ChuPhug <114456408+ChuPhug@users.noreply.github.com>
2023-01-13 14:24:23 +0900

    Add new project Education

---
+++ admin/dashboard.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+
+    <!-- Customer css file -->
+    <link rel="stylesheet" href="css/admin_style.css">
+
+</head>
+
+<body>
+
+
+    <!-- Header section link -->
+    <?php include '../components/admin_header.php';
+?>
+
+
+
+    <!-- Footer section link -->
+    <?php include '../components/admin_footer.php';
+?>
+
+
+
+    <!-- JavaScript file -->
+    <script src="js/admin_script.js"> </script>
+</body>
+
+</html>
